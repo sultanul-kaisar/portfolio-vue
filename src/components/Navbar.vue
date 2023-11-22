44d7e4776ab9dd756169f3042e87b05326b5b36e
sultanul-kaisar <sultanulkaisar@gmail.com>
2023-11-22 19:08:09 +0600

    Project repost

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+   <div id="menu" class="header__menu">
+    <!-- Navigation Start -->
+    <nav class="menu__container">
+      <ul class="navigation">
+        <li>
+          <a id="about-trigger" class="btn btn-default btn-navigation hover-anim hover-secondary" href="#0">
+            <em></em>
+            <span class="btn-caption">About Me</span>
+          </a>
+        </li>
+        <li>
+          <a id="works-trigger" class="btn btn-default btn-navigation hover-anim hover-secondary" href="#0">
+            <em></em>
+            <span class="btn-caption">Portfolio</span>
+          </a>
+        </li>
+        <li>
+          <a id="contact-trigger" class="btn btn-default btn-navigation hover-anim hover-secondary" href="#0">
+            <em></em>
+            <span class="btn-caption">Contact</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- Navigation End -->
+   </div>
+  </template>
+  
+  <script>
+  export default {
+   name: 'Navbar',
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your CSS styles here */
+  </style>
+  
